aba020b7819fd4af66dad7fd1ac6e27168ce4dc7
ShruthinSatheesh300 <vishnuofch@gmail.com>
2021-12-20 00:30:43 +0530

    Added : Code to find the largest number among 3 numbers

---
+++ Largestof3num.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+class Largestof3num
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter 3 number :");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int large;
+		if(a>b)
+			large = a;
+		else
+			large = b;
+		if (c>large)
+		large = c;
+		System.out.println("Largest number among 3 numbers :"+ large);
+	}
+}
